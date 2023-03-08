ef626ef107d5b37d1d43c83fb2f000044025231f
Daniel <daniel.sprcp@gmail.com>
2023-03-07 21:17:47 -0300

    typescript-ii

--- src/database.ts
+++ src/database.ts
@@ -1,32 +1,75 @@
-import { TUser, TProduct, TPurchase } from "./types";
+import { TUser, TProduct, TPurchase, CATEGORY } from "./types";
 
-export const user: TUser [] = [
+export const user: TUser[] = [
     {
         id: "Daniel",
-        email:"daniel@email.com",
-        password: "451278" 
+        email: "daniel@email.com",
+        password: "451278"
     }, {
         id: "Jhon",
-        email:"jhon@email.com",
-        password: "469878"  
+        email: "jhon@email.com",
+        password: "469878"
     }
 ]
 
-export const product: TProduct [] = [
+export function createUser(id: string, email: string, password: string) {
+
+    const newUser: TUser = { id, email, password }
+
+    user.push(newUser)
+
+    console.log("Usuário cadastrado com sucesso")
+}
+
+export function getAllUsers(): void {
+    console.table(user)
+}
+
+export const product: TProduct[] = [
     {
         id: "0101",
         name: "Camiseta",
         price: 30,
-        category: "Vestuário"
+        category: CATEGORY.CLOTHES_AND_SHOES
     }, {
         id: "0202",
         name: "Televisão",
         price: 2500,
-        category: "Eletrodomésticos"
+        category: CATEGORY.ELECTRONICS
     }
 ]
 
-export const purchase: TPurchase [] = [
+export function createProduct(id: string, name: string, price: number, category: CATEGORY) {
+
+    const newProduct: TProduct = { id, name, price, category }
+
+    product.push(newProduct)
+
+    console.log("Produto cadastrado com sucesso")
+
+}
+
+export function getAllProducts(): void {
+
+    console.table(product)
+
+}
+
+export function queryProductsByName(q: string): TProduct[] {
+
+    return product.filter((item) => item.name.toLowerCase().includes(q.toLowerCase()))
+
+}
+
+export function getProductById(id: string) {
+    product.find((item) => {
+        if (item.id === id) {
+            return console.table(item)
+        }
+    })
+}
+
+export const purchase: TPurchase[] = [
     {
         userId: "Daniel",
         productId: "0202",
@@ -39,3 +82,24 @@ export const purchase: TPurchase [] = [
         totalPrice: 90
     }
 ]
+
+export function createPurchase(userId: string, productId: string, quantity: number, totalPrice: number): string {
+
+    purchase.push({ userId, productId, quantity, totalPrice })
+
+    return "Compra realizada com sucesso"
+}
+
+export function getAllPurchasesFromUserId(userIdToSearch: string): TPurchase[] {
+
+    return purchase.filter((item) => 
+
+        item.userId === userIdToSearch
+    )
+}
+
+
+
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
-import { user, product, purchase } from "./database";
+import { user, product, purchase, createUser, getAllUsers, createProduct, getAllProducts, getProductById, queryProductsByName, createPurchase, getAllPurchasesFromUserId } from "./database";
+import { CATEGORY } from "./types";
 
 console.log("O aplicativo foi iniciado")
 
 // EXERCÍCIO 3 - TYPESCRIPT I
 
-console.log(user, product, purchase)
+
+createUser("João", "João@email.com","452879")
+
+getAllUsers()
+
+createProduct("0303","Calça",50,CATEGORY.CLOTHES_AND_SHOES)
+
+getAllProducts()
+
+getProductById("0202")
+
+console.table(queryProductsByName("televisão"))
+
+createPurchase("Daniel", "0202", 2, 5000)
+
+console.log(getAllPurchasesFromUserId("Jhon"))
+
+// console.log(user, product, purchase)
